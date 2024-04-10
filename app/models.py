ff44c1138bd4e8d9eb4c5cc707b8a32e63e1695f
jrobe <jrobe187@uncc.edu>
2024-04-10 19:18:05 -0400

    updated for erd

--- app/models.py
+++ app/models.py
@@ -13,17 +13,39 @@ class User(db.Model, UserMixin):
     spotify_id = db.Column(db.String(120), unique=True, nullable=True)
     spotify_access_token = db.Column(db.String(255), nullable=True)
     spotify_refresh_token = db.Column(db.String(255), nullable=True)
+
     posts = db.relationship('Post', backref='author', lazy=True)
+    streams = db.relationship('Stream', backref='streamer', lazy=True)
 
     def __repr__(self):
         return f'<User {self.username}>'
+    
 
-class Genre(db.Model):
+class Song(db.Model):
+    __tablename__ = 'song'
+
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False, unique=True)
+    spotify_song_id = db.Column(db.String(22), nullable=False)
+    num_streams = db.Column(db.Integer, nullable=False)
+
+    song = db.relationship('song', backref='listener', lazy=True)
+
 
     def __repr__(self):
-        return '<Genre %r>' % self.name
+        return '<Song %r>' % self.spotify_song_id
+
+class Stream(db.Model):
+    __tablename__ = 'stream'
+
+    id = db.Column(db.Integer, db.ForeignKey('song.id'), primary_key=True)
+    spotify_song_id = db.Column(db.String(22))
+    time = db.Column(db.float, nullable=False, unique=True)
+    user = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    def __repr__(self):
+        return '<Stream %r>' % self.id
+    
+
 
 class Artist(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -31,28 +53,6 @@ class Artist(db.Model):
 
     def __repr__(self):
         return '<Artist %r>' % self.name
-
-class Album(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    release_date = db.Column(db.Date, nullable=False)
-    artist_id = db.Column(db.Integer, db.ForeignKey('artist.id'), nullable=False)
-    artist = db.relationship('Artist', backref=db.backref('albums', lazy=True))
-
-    def __repr__(self):
-        return '<Album %r>' % self.title
-
-class Song(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    album_id = db.Column(db.Integer, db.ForeignKey('album.id'), nullable=False)
-    album = db.relationship('Album', backref=db.backref('songs', lazy=True))
-    genre_id = db.Column(db.Integer, db.ForeignKey('genre.id'), nullable=True)
-    genre = db.relationship('Genre', backref=db.backref('songs', lazy=True))
-    duration = db.Column(db.Integer, nullable=False)  # Duration in seconds
-
-    def __repr__(self):
-        return '<Song %r>' % self.title
 
 
 
